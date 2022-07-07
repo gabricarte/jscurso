136be9fde9f42d4ce7431c0af263b162f5791832
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2022-07-07 14:30:24 -0300

    curso js

---
+++ aula12/ex012.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora > 12 && hora < 18) {
+    console.log('Boa tarde! ')
+}
+else if (hora < 6) {
+    console.log('Boa madrugada!')
+}
+else if (hora > 18) {
+    console.log('Boa noite!')
+}
+else {
+    console.log('Bom dia!')
+}
