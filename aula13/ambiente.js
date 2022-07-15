74df250f83dd405dd0b2de9cad00d04ae5374a49
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2022-07-15 14:13:50 -0300

    curso js

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1 // Estrutura de repetição com teste lógico no final
+do {
+    console.log('Tudo bem?')
+    c++ // c = c + c
+} while ((c <= 2))
+
+
+/* Estrutura de repetição com teste lógico no início 
+var c = 1
+while (c <= 2) {
+    console.log('Tudo bem?')
+    c++ // c = c + c
+}
+*/
