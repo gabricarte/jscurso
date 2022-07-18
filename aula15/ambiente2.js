0815c04ad84138f4e94c08d6bff1d56bea26adc3
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2022-07-18 14:09:52 -0300

    curso js

---
+++ aula15/ambiente2.js
@@ -0,0 +1,13 @@
+/* let num = [5, 8, 2, 9, 3]
+
+for (let pos = 0; pos <= num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+} */
+
+// Forma simplificada de percorrer uma lista utilizando o for: 
+
+let num = [5, 8, 2, 9, 3]
+
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
